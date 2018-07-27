2e38b0c1717bce41faafaae09610f335caeef249
kindermoumoute <kindermoumoute@gmail.com>
2018-07-27 13:58:07 +0200

    Deploy to quay.io

---
+++ scripts/build-and-publish.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+if [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_TAG" != "" ]]; then
+  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o blindbot .
+  docker login -u="kindermoumoute" -p="$DOCKER_PASS" quay.io
+  REPO="quay.io/kindermoumoute/blindbot"
+  docker build -f Dockerfile -t $REPO:latest .
+  if [[ "$TRAVIS_TAG" != "" ]]; then
+    docker tag $REPO:latest $REPO:$TRAVIS_TAG
+  fi
+  docker push $REPO
+fi
